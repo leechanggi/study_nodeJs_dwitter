ef6ae8ac5ce8a78270fb8fb82885670eff4ab610
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-03-15 17:51:45 +0900

    add photo

--- 0701_photo/app.js
+++ 0701_photo/app.js
@@ -1,16 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const pathTest = path.join(__dirname, 'test');
-const pathPicture = path.join(__dirname, 'picture');
-const pathVideo = path.join(__dirname, 'picture', 'video');
-const pathCaptured = path.join(__dirname, 'picture', 'captured');
-const pathDuplicated = path.join(__dirname, 'picture', 'duplicated');
-
-function copyFile(org, dest, file) {
-  const readStream = fs.createReadStream(path.join(org, file.name));
-  const piping = readStream.pipe(fs.createWriteStream(path.join(dest, file.name)));
-}
+const pathTest = path.join(__dirname, "test");
+const pathPicture = path.join(__dirname, "picture");
+const pathVideo = path.join(__dirname, "picture", "video");
+const pathCaptured = path.join(__dirname, "picture", "captured");
+const pathDuplicated = path.join(__dirname, "picture", "duplicated");
 
 !fs.existsSync(pathPicture) && fs.mkdirSync(pathPicture);
 !fs.existsSync(pathVideo) && fs.mkdirSync(pathVideo);
@@ -19,20 +14,27 @@ function copyFile(org, dest, file) {
 
 fs.promises
   .readdir(pathTest, { withFileTypes: true })
-  .then(files => {
-    files.forEach(file => {
+  .then((files) => {
+    files.forEach((file) => {
       let ext = path.extname(file.name);
-      if (ext == '.mp4' || ext == '.mov') {
+      if (ext == ".mp4" || ext == ".mov") {
         copyFile(pathTest, pathVideo, file);
-      } else if (ext == '.png' || ext == '.aae') {
+      } else if (ext == ".png" || ext == ".aae") {
         copyFile(pathTest, pathCaptured, file);
       } else if (/^IMG_E/i.test(file.name)) {
         copyFile(pathTest, pathDuplicated, file);
       } else {
-        return false;
+        return;
       }
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
+
+function copyFile(org, dest, file) {
+  const readStream = fs.createReadStream(path.join(org, file.name));
+  const piping = readStream.pipe(
+    fs.createWriteStream(path.join(dest, file.name))
+  );
+}
