ba256a4e214dfeb793be37b61bc78c3542d50f2e
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-03-17 18:01:28 +0900

    addStudy

--- 0902_node-server/2-template.js
+++ 0902_node-server/2-template.js
@@ -1,9 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const ejs = require("ejs");
 // const http2 = require("http2"); // https, 개발환경에서는 사용되지 않음. 배포 시 변경
 
 // console.log(http.STATUS_CODES);
 // console.log(http.METHODS);
+
+const name = "이창기";
+const courses = [
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JavaScript" },
+  { name: "NodeJs" },
+  { name: "EJS" },
+];
 
 const server = http.createServer((req, res) => {
   console.log("incoming...");
@@ -14,16 +24,19 @@ const server = http.createServer((req, res) => {
   // console.log(req.url);
 
   const url = req.url;
-
+  res.setHeader("Content-Type", "text/html");
   if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    fs.createReadStream("./html/index.html").pipe(res);
+    ejs
+      .renderFile("./template/index.ejs", { name })
+      .then((data) => res.end(data));
   } else if (url === "/courses") {
-    res.setHeader("Content-Type", "text/html");
-    fs.createReadStream("./html/courses.html").pipe(res);
+    ejs
+      .renderFile("./template/courses.ejs", { name, courses })
+      .then((data) => res.end(data));
   } else {
-    res.setHeader("Content-Type", "text/html");
-    fs.createReadStream("./html/not-found.html").pipe(res);
+    ejs
+      .renderFile("./template/not-found.ejs", { name })
+      .then((data) => res.end(data));
   }
 
   // res.end(); // pipe() 에서 res.end()를 처리해주기 때문에 중복으로 작성해주지 않아도 된다!
