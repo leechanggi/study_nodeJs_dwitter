03e461f1ad7c568ce9e31622f936ffed1d2a455e
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-03-22 23:59:00 +0900

    add

--- node/1100_express/app.js
+++ node/1100_express/app.js
@@ -1,19 +1,21 @@
-import express from "express";
+import express from 'express';
+import fs, { fsync } from 'fs';
+
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
 });
 
-app.post("/", (req, res, next) => {
+app.post('/', (req, res, next) => {
   console.log(req.body);
 });
 
-// ** Req && Res Method **
-app.get("/board/:id", (req, res, next) => {
+// ** 1, Req && Res Method **
+app.get('/board/:id', (req, res, next) => {
   console.log(req.path);
   console.log(req.headers);
   console.log(req.params);
@@ -28,56 +30,66 @@ app.get("/board/:id", (req, res, next) => {
   res.send(`board ${req.params.id}`);
 });
 
-// ** Next Method **
+// ** 2, Next Method **
 app.get(
-  "/viewer/:id",
+  '/viewer/:id',
   (req, res, next) => {
-    console.log("first");
+    console.log('first');
     next();
-    // next("route");
+    // next("route"); // => console.log('first2') 를 건너뜀.
     // next(new Error("error"));
   },
   (req, res, next) => {
-    console.log("first2");
+    console.log('first2');
     next();
   }
 );
-app.get("/viewer/:id", (req, res, next) => {
-  console.log("second");
-  res.send("this is middleware!");
+app.get('/viewer/:id', (req, res, next) => {
+  console.log('second');
+  res.send('this is middleware!');
 });
 
-// ** All Method **
-app.all("/api", (req, res, next) => {
+// ** 3, All Method **
+app.all('/api', (req, res, next) => {
   // ** all은 "/api" 특정한 경로에만 미들웨어를 수행, all 과 use 둘다 사용시에는 all 먼저 수행
-  console.log("all api/");
+  console.log('all api/');
   next();
 });
-app.all("/api/*", (req, res, next) => {
-  console.log("all api/*");
+
+app.all('/api/*', (req, res, next) => {
+  console.log('all api/*');
   next();
 });
-app.use("/api", (req, res, next) => {
+
+app.use('/api', (req, res, next) => {
   // ** use은 "/api/~" 경로에 모두 미들웨어를 수행
   console.log(req.params.id);
   if (req.params.id) {
-    ck;
-    return res.send("this is conditional... all and use method!");
+    return res.send('this is conditional... all and use method!');
     // 조건부 send 사용시에 return 해서 send가 중복되지 않게해야만 한다.
   }
-  console.log("use api/*");
-  res.send("this is all and use method!");
+  console.log('use api/*');
+  res.send('this is all and use method!');
 });
 
-// ** MiddleWare used for Error **
+// ** 4, Async Error handleing(비동기적 에러 처리)
+app.get('/file', (req, res, next) => {
+  fs.readFile('./file1.txt', (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+    }
+  });
+});
+
+// ** 5, MiddleWare used for Error **
 app.use((req, res, next) => {
-  res.status(404).send("Not found page!");
+  res.status(404).send('Not found page!');
 });
 app.use((error, req, res, next) => {
   console.error(error);
-  res.status(500).send("Sorry, try later!");
+  res.status(500).send('Sorry, try later!');
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`_______________________________\nlistening http://localhost:${port}`);
 });
