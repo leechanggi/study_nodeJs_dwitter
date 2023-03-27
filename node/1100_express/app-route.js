1f4c5c9ddde86a5a16f27f4818936a66732aae23
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-03-27 17:42:52 +0900

    add 1200

--- node/1100_express/app-route.js
+++ node/1100_express/app-route.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser"; //요청된 쿠키를 쉽게 추출할 수 있도록 도와줌
+import morgan from "morgan"; //요청과 응답에 대한 정보를 콘솔에 기록
+import helmet from "helmet";
 
 import postRouter from "./router/post.js";
 import userRouter from "./router/user.js";
@@ -16,28 +19,34 @@ const options = {
     res.set("x-timestamp", Date.now());
   },
 };
+const corsOptions = {
+  origin: ["http://127.0.0.1:5500"],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
-app.use(express.json()); // REST API, Body
-app.use(express.urlencoded({ extended: false })); //HTML Form
-app.use(express.static("public", options));
-
+app.use(express.json());
+app.use(cookieParser()); // cookie-parser
+app.use(morgan("tiny")); // morgan
+app.use(helmet());
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
 //   res.setHeader(
-//     "Access-Control-Allow-Origin",
+//     "Access-Control-Allow-Methods",
 //     "OPTIONS, GET, POST, PUT, DELETE"
 //   );
 //   next();
 // });
+app.use(cors(corsOptions));
 
-// npm install cors
+app.use(express.urlencoded({ extended: false })); //HTML Form
+app.use(express.static("public", options));
 
-app.use(
-  cors({
-    origin: ["http://127.0.0.1:5500"],
-  })
-);
-
+app.get("/", (req, res, next) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  res.send("Welcome");
+});
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
 
