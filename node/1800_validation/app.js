528740455b806d70da1b6fc89f5bd33d866fc302
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-04-06 18:00:36 +0900

    Update app.js

--- node/1800_validation/app.js
+++ node/1800_validation/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { body, param, validationResult } from 'express-validator';
+import express from "express";
+import { body, param, validationResult } from "express-validator";
 
 const app = express();
 app.use(express.json());
@@ -9,16 +9,31 @@ const validate = (req, res, next) => {
   if (errors.isEmpty()) {
     return next();
   }
-  return res.status(400).json({ message: errors.array()[0].msg });
+  return res.status(400).json({ message: errors.array() });
 };
 
 app.post(
-  '/users',
+  "/users",
   [
-    body('name').trim().isLength({ min: 2 }).withMessage('이름은 두글자 이상!'),
-    body('age').isInt().withMessage('숫자를 입력해'),
-    body('email').isEmail().withMessage('이메일 입력해요').normalizeEmail(),
-    body('job.name').notEmpty(),
+    body("name")
+      .trim() // Sanitization(Validation을 위한 전처리), trim() -> 공백제거
+      .notEmpty()
+      .withMessage("이름을 입력해 주세요.")
+      .isLength({ min: 2, max: 6 })
+      .withMessage("이름은 2자 이상, 6자 이하로 입력해 주세요."),
+    body("age")
+      .notEmpty()
+      .withMessage("나이를 입력해 주세요.")
+      .isInt()
+      .withMessage("숫자를 입력해 주세요."),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("이메일을 입력해 주세요.")
+      .isEmail()
+      .withMessage("이메일 형식에 맞춰 입력해 주세요.")
+      .normalizeEmail(), // 이메일을 소문자로
+    body("job.name").notEmpty(),
     validate,
   ],
   (req, res, next) => {
@@ -28,10 +43,13 @@ app.post(
 );
 
 app.get(
-  '/:email',
-  [param('email').isEmail().withMessage('이메일 입력해요'), validate],
+  "/:email",
+  [
+    param("email").isEmail().withMessage("이메일 형식에 맞춰 입력해 주세요."),
+    validate,
+  ],
   (req, res, next) => {
-    res.send('💌');
+    res.send("💌");
   }
 );
 
