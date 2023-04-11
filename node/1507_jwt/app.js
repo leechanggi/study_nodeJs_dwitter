d3d6f9f25129567169df6c976fc0da011867aaef
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-04-11 17:42:11 +0900

    Update app.js

--- node/1507_jwt/app.js
+++ node/1507_jwt/app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import "express-async-errors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 8080;
@@ -33,17 +34,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public", options));
 
 //
-app.get("/", (req, res) => {
-  const tempPw = "abcd1234";
-  const hashed = bcrypt.hashSync(tempPw, 10); // 8 ~ 12 정도가 적당
-  console.log(`password: ${tempPw}, hashed: ${hashed}`);
+// app.get("/", (req, res) => {
+//   const tempPw = "abcd1234";
+//   const hashed = bcrypt.hashSync(tempPw, 10); // 8 ~ 12 정도가 적당
+//   console.log(`password: ${tempPw}, hashed: ${hashed}`);
 
-  const result = bcrypt.compareSync("abcd1234", hashed);
-  console.log(`compare: ${result}`);
+//   const result = bcrypt.compareSync("abcd1234", hashed);
+//   console.log(`compare: ${result}`);
+
+//   res.send("HOME");
+// });
+
+const secret = "16zd&V2!hbS2*8Zs8oC4Uqd71#uxVkJ0";
+app.get("/", (req, res) => {
+  const token = jwt.sign(
+    {
+      id: "userId",
+      isAdmin: true,
+    },
+    secret,
+    { expiresIn: 2 }
+  );
+  console.log(token);
+
+  setTimeout(() => {
+    jwt.verify(token, secret, (error, decoded) => {
+      console.log(error, decoded);
+    });
+  }, 1000);
+
+  setTimeout(() => {
+    jwt.verify(token, secret, (error, decoded) => {
+      console.log(error, decoded);
+    });
+  }, 3000);
 
   res.send("HOME");
 });
-
 //
 
 app.use((req, res, next) => {
