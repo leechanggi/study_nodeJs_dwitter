c3d078acb4b7251e05cb7c97e03985071c694dc6
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-05-10 21:08:16 +0900

    modi

--- dwitter/client/src/components/Tweets.jsx
+++ dwitter/client/src/components/Tweets.jsx
@@ -1,70 +1,62 @@
-import React, { memo, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import Banner from "./Banner";
-import NewTweetForm from "./NewTweetForm";
-import TweetCard from "./TweetCard";
-import { useAuth } from "../context/AuthContext";
+import React, { memo, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Banner from './Banner';
+import NewTweetForm from './NewTweetForm';
+import TweetCard from './TweetCard';
+import { useAuth } from '../context/AuthContext';
 
 const Tweets = memo(({ tweetService, username, addable }) => {
   const [tweets, setTweets] = useState([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const history = useHistory();
   const { user } = useAuth();
 
   useEffect(() => {
     tweetService
       .getTweets(username)
-      .then((tweets) => setTweets([...tweets]))
+      .then(tweets => setTweets([...tweets]))
       .catch(onError);
 
-    const stopSync = tweetService.onSync((tweet) => onCreated(tweet));
+    const stopSync = tweetService.onSync(tweet => onCreated(tweet));
     return () => stopSync();
   }, [tweetService, username, user]);
 
-  const onCreated = (tweet) => {
-    setTweets((tweets) => [tweet, ...tweets]);
+  const onCreated = tweet => {
+    setTweets(tweets => [tweet, ...tweets]);
   };
 
-  const onDelete = (tweetId) =>
+  const onDelete = tweetId =>
     tweetService
       .deleteTweet(tweetId)
-      .then(() =>
-        setTweets((tweets) => tweets.filter((tweet) => tweet.id !== tweetId))
-      )
-      .catch((error) => setError(error.toString()));
+      .then(() => setTweets(tweets => tweets.filter(tweet => tweet.id !== tweetId)))
+      .catch(error => setError(error.toString()));
 
   const onUpdate = (tweetId, text) =>
     tweetService
       .updateTweet(tweetId, text)
-      .then((updated) =>
-        setTweets((tweets) =>
-          tweets.map((item) => (item.id === updated.id ? updated : item))
-        )
+      .then(updated =>
+        setTweets(tweets => tweets.map(item => (item.id === updated.id ? updated : item)))
       )
-      .catch((error) => error.toString());
+      .catch(error => error.toString());
 
-  const onUsernameClick = (tweet) => history.push(`/${tweet.username}`);
+  const onUsernameClick = tweet => history.push(`/${tweet.username}`);
 
-  const onError = (error) => {
+  const onError = error => {
     setError(error.toString());
     setTimeout(() => {
-      setError("");
+      setError('');
     }, 3000);
   };
 
   return (
     <>
       {addable && (
-        <NewTweetForm
-          tweetService={tweetService}
-          onError={onError}
-          onCreated={onCreated}
-        />
+        <NewTweetForm tweetService={tweetService} onError={onError} onCreated={onCreated} />
       )}
       {error && <Banner text={error} isAlert={true} transient={true} />}
       {tweets.length === 0 && <p className="tweets-empty">No Tweets Yet</p>}
       <ul className="tweets">
-        {tweets.map((tweet) => (
+        {tweets.map(tweet => (
           <TweetCard
             key={tweet.id}
             tweet={tweet}
--- dwitter/client/src/index.js
+++ dwitter/client/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import AuthService from "./service/auth";
-import TweetService from "./service/tweet";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { AuthErrorEventBus } from "./context/AuthContext";
-import HttpClient from "./network/http";
-import Socket from "./network/socket";
-import TokenStorage from "./db/token";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import AuthService from './service/auth';
+import TweetService from './service/tweet';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { AuthErrorEventBus } from './context/AuthContext';
+import HttpClient from './network/http';
+import Socket from './network/socket';
+import TokenStorage from './db/token';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 const tokenStorage = new TokenStorage();
@@ -22,13 +22,10 @@ const tweetService = new TweetService(httpClient, tokenStorage, socketClient);
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider
-        authService={authService}
-        authErrorEventBus={authErrorEventBus}
-      >
+      <AuthProvider authService={authService} authErrorEventBus={authErrorEventBus}>
         <App tweetService={tweetService} />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
